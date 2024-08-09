74cae22a66b01d767a3ffe24cac295f9a21188b8
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-09 14:45:10 +0200

    fixed rquirements conditions

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,9 +9,9 @@
 void selection_sort(int *array, size_t size)
 {
 size_t i, j, min_idx;
-int temp;
+size_t temp;
 
-if (array == NULL || size < 2)
+if (array == NULL || size < 1)
 return;
 
 for (i = 0; i < size - 1; i++)
