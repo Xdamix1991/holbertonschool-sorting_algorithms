87358c3c551f76f1b640c965570c1a61d506aca9
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-09 14:58:19 +0200

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 size_t i, j, min_idx;
 size_t temp;
 
-if (array == NULL || size < 1)
+if (array == NULL || size == 0)
 return;
 
 for (i = 0; i < size - 1; i++)
